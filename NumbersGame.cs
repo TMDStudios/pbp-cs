8376feaf20dea16f339417ccfb61c3fef9b6667f
TMDStudios <tmdstudioscontact@gmail.com>
2024-03-06 22:20:12 -0500

    Initial Commit

---
+++ NumbersGame.cs
@@ -0,0 +1,51 @@
+class NumbersGame{
+    int Answer;
+    int Count;
+    public NumbersGame(){
+        Console.WriteLine("Loading Numbers Game");
+    }
+
+    void GameSetup(){
+        Answer=new Random().Next(0,11);
+        Count=0;
+    }
+
+    public void StartGame(){
+        GameSetup();
+        Console.WriteLine("Starting Numbers Game");
+        Console.WriteLine("To start press any key");
+        Console.ReadLine();
+        while(Count<3){
+            Console.WriteLine("Guess a number between 0 and 10:");
+            string UserInput = Console.ReadLine();
+            try{
+                int Guess = Int32.Parse(UserInput);
+                if(Guess<0 || Guess>10){
+                    Console.WriteLine("You can only guess numbers between 0 and 10.");
+                    continue;
+                }
+                Count++;
+                if(Guess==Answer){
+                    Console.WriteLine($"You got it! The number was {Answer}. You guessed {Count} times.");
+                    break;
+                }else{
+                    if(Count>=3){
+                        Console.WriteLine("You lose.");
+                    }else{
+                        Console.WriteLine($"Try again.\nYou have guessed {Count} time(s).");
+                    }
+                }
+            }catch{
+                Console.WriteLine("You can only guess number.");
+            }
+        }
+
+        Console.WriteLine("Game Finished\nType 'y' to play again or press any other key to quit.");
+        string PlayAgain = Console.ReadLine();
+        if(PlayAgain.ToLower()=="y"){
+            StartGame();
+        }else{
+            Console.WriteLine("Shutting Down Numbers Game");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿bool Exit = false;
+string[] ConsoleOptions = {
+    "Choose from the following menu:",
+    "1 => Numbers Game",
+    "2 => Calculator",
+    "3 => Guess the Phrase",
+    "4 => Username and Password",
+    "5 => Quit"
+    };
+NumbersGame numbersGame = new();
+
+void PrintWelcome(){
+    string Message = "This is a collection of select Project Based Python projects";
+    for(int i = 0; i<Message.Length; i++){
+        Console.Write("*");
+    }
+    Console.WriteLine($"\n{Message}");
+    for(int i = 0; i<Message.Length; i++){
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+    for(int i = 0; i<ConsoleOptions.Length; i++){
+        Console.WriteLine(ConsoleOptions[i]);
+    }
+    Console.Write(">>");
+}
+
+while(!Exit){
+    PrintWelcome();
+    switch(Console.ReadLine()){
+        case "1":
+            numbersGame.StartGame();
+            break;
+        case "2":
+            Console.WriteLine("Coming Soon");
+            break;
+        case "3":
+            Console.WriteLine("Coming Soon");
+            break;
+        case "4":
+            Console.WriteLine("Coming Soon");
+            break;
+        case "5":
+            Exit=true;
+            break;
+        default:
+            Console.WriteLine("Please select a valid option");
+            break;
+    }
+}
+Console.WriteLine("Closing Program");
